bad76829aba68c7c688c1a76dbd92c4923cc9ad8
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-07-29 21:04:49 -0300

    [*] added app provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import Routes from './routes'
 import GlobalStyle from './styles/global'
+import AppProvider from './context'
 import { ToastContainer } from 'react-toastify'
 
 function App() {
   return (
-    <>
+    <AppProvider>
       <Routes />
       <ToastContainer autoClose={2000} theme="colored" />
       <GlobalStyle />
-    </>
+    </AppProvider>
   )
 }
 
